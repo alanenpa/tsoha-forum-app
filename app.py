31edfdbb7c66c02941afc88ff5ac052d28d677cd
alanenpa <72764484+alanenpa@users.noreply.github.com>
2021-09-26 23:28:33 +0300

    fix Heroku Postgres connection

--- app.py
+++ app.py
@@ -4,10 +4,17 @@ from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import check_password_hash, generate_password_hash
 from os import getenv
 
+import os
+import re
+
 app = Flask(__name__)
+uri = os.getenv("DATABASE_URL")
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = getenv("SECRET_KEY")
+
 db = SQLAlchemy(app)
 
 
