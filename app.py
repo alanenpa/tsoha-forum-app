4bb83a6450ccbb7ee0b04f884572485a16b84cb0
alanenpa <72764484+alanenpa@users.noreply.github.com>
2021-09-27 23:26:54 +0300

    testing heroku functionality

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ app = Flask(__name__)
 uri = os.getenv("DATABASE_URL")
 if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
+print('uri:', uri)
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv(uri)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = getenv("SECRET_KEY")
