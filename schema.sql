1d1b915038e7815758ac48ddfbe610a9dcaef2c7
alanenpa <72764484+alanenpa@users.noreply.github.com>
2021-10-06 18:21:47 +0300

    add schema.sql

---
+++ schema.sql
@@ -0,0 +1,29 @@
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username TEXT UNIQUE, 
+    password TEXT,
+    admin BOOLEAN
+);
+
+CREATE TABLE topics (
+    id SERIAL PRIMARY KEY,
+    topic TEXT
+);
+
+CREATE TABLE threads (
+    id SERIAL PRIMARY KEY,
+    topic_id INTEGER REFERENCES topics,
+    user_id INTEGER REFERENCES users,
+    header TEXT,
+    init_msg TEXT,
+    created_at TIMESTAMP,
+);
+
+CREATE TABLE messages (
+    id SERIAL PRIMARY KEY,
+    thread_id INTEGER REFERENCES threads,
+    user_id INTEGER REFERENCES users,
+    content TEXT,
+    sent_at TIMESTAMP,
+);
