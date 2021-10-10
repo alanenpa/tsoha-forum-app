088a1c36f5b2a2103b85880099c596a16532c31c
alanenpa <72764484+alanenpa@users.noreply.github.com>
2021-10-10 21:40:36 +0300

    create topics.py

---
+++ topics.py
@@ -0,0 +1,10 @@
+from db import db
+
+def create_topic(topic):
+    sql = "INSERT INTO topics (topic) VALUES (:topic)"
+    db.session.execute(sql, {"topic": topic})
+    db.session.commit()
+
+def get_all():
+    result = db.session.execute("SELECT * FROM topics")
+    return result.fetchall()
