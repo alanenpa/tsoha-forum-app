974026b2975c5d33445056f2c663a7bb0bdd4ac2
alanenpa <72764484+alanenpa@users.noreply.github.com>
2021-10-10 21:40:21 +0300

    create users.py

---
+++ users.py
@@ -0,0 +1,45 @@
+from db import db
+from werkzeug.security import check_password_hash, generate_password_hash
+from flask import session
+
+def get_by_id(user_id):
+    sql = "SELECT * FROM users WHERE id=:id"
+    result = db.session.execute(sql, {"id": user_id})
+    return result.fetchone()
+
+def sign_in(username, password):
+    sql = "SELECT * FROM users WHERE username=:username"
+    result = db.session.execute(sql, {"username": username})
+    user = result.fetchone()
+    if not user:
+        return False
+    else:
+        if check_password_hash(user.password, password):
+            session["user_id"] = user.id
+            return True
+        else:
+            return False
+
+def sign_up(username, password, admin):
+    passwordHash = generate_password_hash(password)
+    try:
+        sql = "INSERT INTO users (username, password, admin) VALUES (:username, :password, :admin)"
+        db.session.execute(sql, {"username": username, "password": passwordHash, "admin": admin})
+        db.session.commit()
+    except:
+        return False
+    return sign_in(username, password)
+
+def user_id():
+    return session.get("user_id", 0)
+
+def is_admin():
+    id = user_id()
+    if id == 0:
+        return False
+    sql = "SELECT admin FROM users WHERE id=:id"
+    result = db.session.execute(sql, {"id": id})
+    return result.fetchone()[0]
+
+def sign_out():
+    del session["user_id"]
